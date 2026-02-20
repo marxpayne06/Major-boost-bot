d70865aa36112007464046ae8fb394e592d19943
marxpayne06 <marxppssppgameplay@gmail.com>
2026-02-20 14:12:10 +0100

    Modify run_flask to use dynamic port
    Update Flask app to use PORT environment variable.

--- main.py
+++ main.py
@@ -11,9 +11,13 @@ app = Flask('')
 @app.route('/')
 def home():
     return "Bot is alive!"
+import os # Make sure to add this import at the very top of your file
 
 def run_flask():
-    app.run(host='0.0.0.0', port=8080)
+    # Render provides a PORT environment variable; defaults to 10000 if not found
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
+
 
 def keep_alive():
     t = threading.Thread(target=run_flask)
